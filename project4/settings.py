d4336228f3090c3c3327081c46b4578854c50727
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2025-01-08 00:49:49 +0800

    refactor: change DEBUG MODE TO FALSE

--- project4/settings.py
+++ project4/settings.py
@@ -28,7 +28,7 @@ APPS_DIR =  os.path.join(BASE_DIR, "network")
 SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
